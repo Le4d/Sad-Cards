51db93bf9e77341aa3a02a996788bf3cd7a74d8e
Le4d <rasmus_poulsen@outlook.dk>
2022-05-10 09:41:44 +0200

    prøver at lave en draw mechanic

---
+++ Card Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> deck = new List<GameObject>();
+    [SerializeField] Transform[] cardSlots;
+    [SerializeField] bool[] availableCardSlots;
+
+    public void DrawCard()
+    {
+        if (deck.Count >= 1)
+        {
+            GameObject randCard = deck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < availableCardSlots.Length; i++)
+            {
+                if (availableCardSlots[i] == true)
+                {
+                    randCard.gameObject.SetActive(true);
+                    randCard.transform.position = cardSlots[i].position;
+                    availableCardSlots[i] = false;
+                    deck.Remove(randCard);
+                }
+            }
+        }
+    }
+
+    public void Start()
+    {
+        DrawCard();
+    }
+}
